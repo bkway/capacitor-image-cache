57f1ffcd3c80387c55f5e436d4de1d59143c89e1
Kamil Steuer <kamil.steuer@autea.pl>
2020-03-24 07:57:55 +0100

    Improved iOS caching. Switched to Kingfish library.

--- src/core/ios/Plugin/Plugin/Plugin.swift
+++ src/core/ios/Plugin/Plugin/Plugin.swift
@@ -1,53 +1,37 @@
 import Foundation
 import Capacitor
-import SDWebImage
 import Photos
+import Kingfisher
+
 let KEY = "_CAP_IMAGE_CACHE_"
 typealias JSObject = [String:Any]
 @objc(ImageCachePlugin)
 public class ImageCachePlugin: CAPPlugin {
     private var cache: NSDictionary?
-    private var manager: SDWebImageManager?
+    private var manager: KingfisherManager?
     public override func load() {
-        manager = SDWebImageManager.shared()
-        manager?.imageDownloader?.shouldDecompressImages = false
+        let imageCache = ImageCache.default
+        imageCache.diskStorage.config.pathExtension = "png"
+        
+        self.manager = KingfisherManager.init(downloader: ImageDownloader.default, cache: ImageCache.default)
     }
 
     @objc func get(_ call: CAPPluginCall) {
         let src = call.getString("src") ?? ""
         if(src.contains("http:") || src.contains("https:")){
-            let url = URL.init(string: src )
-
-            self.manager?.loadImage(with: url, options: SDWebImageOptions.scaleDownLargeImages, progress: { (receivedSize, expectedSize, path) in
-
-
-            }, completed: { (image, data, error, type, finished, completedUrl) in
-
-                if(image == nil && error != nil && data == nil){
-                    call.reject(error!.localizedDescription)
-                }else if(finished && completedUrl != nil ){
-                    if(type == SDImageCacheType.disk){
-                        DispatchQueue.main.async {
-                        let key = self.manager?.cacheKey(for: completedUrl)
-                        let source = self.manager?.imageCache?.defaultCachePath(forKey: key)
-                        var obj = JSObject()
-                        obj["value"] = CAPFileManager.getPortablePath(host: self.bridge!.getLocalUrl(), uri: URL.init(string: source!));
-                        call.resolve(obj)
-                        }
-                    }else{
-                        SDImageCache.shared().store(image, forKey: completedUrl?.absoluteString, completion: {
-                            DispatchQueue.main.async {
-                                let key = self.manager?.cacheKey(for: completedUrl)
-                                let source = self.manager?.imageCache?.defaultCachePath(forKey: key)
-                                var obj = JSObject()
-                                obj["value"] = CAPFileManager.getPortablePath(host: self.bridge!.getLocalUrl(), uri: URL.init(string: source!));
-                                call.resolve(obj)
-                            }
-                        })
-                    }
-                }
-
-            })
+            var path = src
+            DispatchQueue.main.async {
+                if (self.manager!.cache.isCached(forKey: src)) {
+                   let systemPath = self.manager!.cache.cachePath(forKey: src)
+                   path = CAPFileManager.getPortablePath(host: self.bridge!.getLocalUrl(), uri: URL.init(string: systemPath))!;
+               } else {
+                   self.saveImage(src: path)
+               }
+               
+              var obj = JSObject()
+              obj["value"] = path
+              call.resolve(obj)
+            }
         }else{
             var obj = JSObject()
             obj["value"] = src;
@@ -58,59 +42,66 @@ public class ImageCachePlugin: CAPPlugin {
 
     @objc func hasItem(_ call: CAPPluginCall) {
         let src = call.getString("src") ?? ""
-        let url = URL.init(string: src)
-        manager?.cachedImageExists(for: url , completion: { (exists) in
-            DispatchQueue.main.async {
-                var obj = JSObject()
-                obj["value"] = exists
-                call.resolve(obj)
-            }
-        })
+        let exists = self.manager!.cache.isCached(forKey: src)
+        DispatchQueue.main.async {
+                       var obj = JSObject()
+                       obj["value"] = exists
+                       call.resolve(obj)
+                   }
     }
 
     @objc func clearItem(_ call: CAPPluginCall) {
         let src = call.getString("src") ?? ""
-        manager?.imageCache?.removeImage(forKey: src, fromDisk: true, withCompletion: {
+        self.manager!.cache.removeImage(forKey: src, processorIdentifier: "", fromMemory: true, fromDisk: true, callbackQueue: .untouch) {
             DispatchQueue.main.async {
                 call.resolve()
             }
-        })
-
+        }
     }
 
 
     @objc func clear(_ call: CAPPluginCall) {
-        manager?.imageCache?.clearMemory()
+        self.manager!.cache.clearDiskCache()
+        self.manager!.cache.clearMemoryCache()
     }
     
     @objc func saveImage(_ call: CAPPluginCall) {
         let src = call.getString("src") ?? ""
         if(src.contains("http:") || src.contains("https:")) {
             let url = URL.init(string: src )
-            self.manager?.loadImage(with: url, options: SDWebImageOptions.fromCacheOnly, progress: { (receivedSize, expectedSize, path) in
-
-            }, completed: { (image, data, error, type, finished, completedUrl) in
-                if(image == nil && error != nil && data == nil){
-                    call.reject(error!.localizedDescription)
-                }else if(finished && completedUrl != nil ){
-                    self.checkAuthorization(allowed: {
-                        // Add it to the photo library.
-                        PHPhotoLibrary.shared().performChanges({
-                            PHAssetChangeRequest.creationRequestForAsset(from: image!)
-                        }, completionHandler: {success, error in
-                            if !success {
-                                call.reject("Unable to save image to camera poll", error)
-                            } else {
-                                call.resolve()
-                            }
-                        })
-                    }, notAllowed: {
-                        call.reject("Access to photos not allowed by user")
+            self.manager!.downloader.downloadImage(with: url!, options: nil) { (result) in
+                switch result {
+                case .success(let value):
+                    self.manager!.cache.store(value.image, forKey: src, options: KingfisherParsedOptionsInfo([.cacheSerializer(FormatIndicatedCacheSerializer.png)]),
+                    toDisk: true,completionHandler: { (result) in
+                        call.resolve()
                     })
+                case .failure(let error):
+                    call.reject(error.localizedDescription)
                 }
-            })
+            }
         } else {
             call.reject("src must use an http or https scheme")
+        }
+
+    }
+    
+    private func saveImage(src: String) {
+        if(src.contains("http:") || src.contains("https:")) {
+            let url = URL.init(string: src )
+            self.manager!.downloader.downloadImage(with: url!, options: nil) { (result) in
+                switch result {
+                case .success(let value):
+                    self.manager!.cache.store(value.image, forKey: src, options: KingfisherParsedOptionsInfo([.cacheSerializer(FormatIndicatedCacheSerializer.png)]),
+                    toDisk: true,completionHandler: { (result) in
+                        print(result)
+                    })
+                case .failure(let error):
+                    print("Cannot save image", error)
+                }
+            }
+        } else {
+             print("Cannot save image","src must use an http or https scheme")
         }
     }
     
